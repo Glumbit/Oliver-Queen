6ec924c3481fce47f5608a8f63d015cedea60c8c
Glumbit <footbolka46@gmail.com>
2023-04-28 13:11:55 +0300

    toggle nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
 const burger = document.querySelectorAll('.burger');
-for (const i in burger) {
-	if (Object.hasOwnProperty.call(burger, i)) {
-		const element = burger[i];
-		element.addEventListener("click", function () {
-			document.querySelector('.navbar-nav-custom').classList.toggle("navbar-nav-custom-active");
-		})
+const nav = document.querySelector('.navbar-nav-custom');
+const navItem = document.querySelectorAll('.nav-item');
+
+showNav(burger)
+showNav(navItem)
+
+function showNav(collection) {
+	for (const i in collection) {
+		if (Object.hasOwnProperty.call(collection, i)) {
+			const element = collection[i];
+			element.addEventListener("click", function () {
+				nav.classList.toggle("navbar-nav-custom-active");
+			})
+		}
 	}
 }
+
+const burgerOpen = document.querySelector('#burger-open');
+
+document.addEventListener('click', (e) => {
+	const insideNav = e.composedPath().includes(nav);
+	const insideBurgerOpen = e.composedPath().includes(burgerOpen);
+	if (!insideNav && !insideBurgerOpen) nav.classList.remove("navbar-nav-custom-active");
+})
